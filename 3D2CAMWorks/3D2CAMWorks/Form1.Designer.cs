b612249eca2d639b6ed69e6be32760c82db5bdae
amitregmi <amitregmi11@gmail.com>
2017-07-15 10:07:23 +0900

    Add files via upload

---
+++ 3D2CAMWorks/3D2CAMWorks/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _3D2CAMWorks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_CAM_Calc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_CAM_Calc
+            // 
+            this.button_CAM_Calc.Location = new System.Drawing.Point(12, 199);
+            this.button_CAM_Calc.Name = "button_CAM_Calc";
+            this.button_CAM_Calc.Size = new System.Drawing.Size(260, 50);
+            this.button_CAM_Calc.TabIndex = 0;
+            this.button_CAM_Calc.Text = "CAM計算";
+            this.button_CAM_Calc.UseVisualStyleBackColor = true;
+            this.button_CAM_Calc.Click += new System.EventHandler(this.button_CAM_Calc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button_CAM_Calc);
+            this.Name = "Form1";
+            this.Text = "3D2CAMWorks";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_CAM_Calc;
+    }
+}
+
---
+++ 3D2CAMWorks/3D2CAMWorks/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using CAMWORKSLib;
+
+namespace _3D2CAMWorks
+{
+    public partial class Form1 : Form
+    {
+        SldWorks swApp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_CAM_Calc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                //Initialization SolidWorks
+                Type swType = Type.GetTypeFromProgID("SldWorks.Application");
+                SldWorks swApp = (SldWorks)Activator.CreateInstance(swType);
+                //Initialization CAMWorks
+                CWApp cwAPP = new CWApp();
+                // Extract Machinable Features by AFR
+                ICWDoc3 cwActiveDoc = cwAPP.IGetActiveDoc();
+                cwActiveDoc.ExtractMachinableFeatures();
+                // Generate Operation                
+                cwAPP.ActiveDocGOP(1);
+                // Generate Tool Path               
+                ICWPartDoc cwPartDoc = (ICWPartDoc)cwActiveDoc;
+                cwPartDoc.GenerateToolpath();
+                // Simulation
+                cwAPP.ActiveDocLaunchSimulation();
+                cwAPP.ActiveDocPlaySimulation();
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
